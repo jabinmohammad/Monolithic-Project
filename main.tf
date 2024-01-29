1b5d30b4623d791aa59fca017fe27c99053488d6
Md. Jabin <144356021+jabinmohammad@users.noreply.github.com>
2024-01-29 21:28:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,4 +14,23 @@ resource "aws_instance" "server" {
     Name = dynamic.tag.0.content.value
   }
 }
-
+resource "aws_s3_bucket" {
+  bucket = "goodatlucks"
+  region = "eu-north-1"
+  #key    = "terraform.tfstate"
+  versioning {
+    enabled = true
+  }
+}
+/*
+terraform {
+  backend "s3" {
+    bucket = "swiggyterraformbucket001"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    versioning {
+    enabled = true
+  }
+  }
+}
+*/
