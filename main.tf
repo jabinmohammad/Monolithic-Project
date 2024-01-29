ca4b9389bffa42e5d6ec8d52335b8493c9b82274
Md. Jabin <144356021+jabinmohammad@users.noreply.github.com>
2024-01-29 21:10:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
-p
+resource "aws_instance" "server" {
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  dynamic "tag" {
+    for_each = var.server_names
+    content {
+      key   = each.key
+      value = each.value
+    }
+  }
+
+  tags = {
+    Name = dynamic.tag.0.content.value
+  }
+}
+
